7e0432f8be7373f3dd5dbf8fbd26db060c550aa3
Kai-Arne Reiter <git@mirrorspock.com>
2014-04-28 18:28:31 +0200

    finished tutorial

--- SpriteKitSimpleGame/MyScene.h
+++ SpriteKitSimpleGame/MyScene.h
@@ -1,11 +1,3 @@
-//
-//  MyScene.h
-//  SpriteKitSimpleGame
-//
-
-//  Copyright (c) 2014 imkai. All rights reserved.
-//
-
 #import <SpriteKit/SpriteKit.h>
 
 @interface MyScene : SKScene
--- SpriteKitSimpleGame/MyScene.m
+++ SpriteKitSimpleGame/MyScene.m
@@ -1,53 +1,181 @@
-//
-//  MyScene.m
-//  SpriteKitSimpleGame
-//
-//  Created by Mirror Spock on 28/04/14.
-//  Copyright (c) 2014 imkai. All rights reserved.
-//
-
+#import "GameOverScene.h"
 #import "MyScene.h"
+
+static const uint32_t projectileCategory     =  0x1 << 0;
+static const uint32_t monsterCategory        =  0x1 << 1;
+
+@interface MyScene () <SKPhysicsContactDelegate>
+@property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
+@property (nonatomic) NSTimeInterval lastUpdateInterval;
+@property (nonatomic) SKSpriteNode * player;
+@property (nonatomic) int monstersDestroyed;
+@end
+
+static inline CGPoint rwAdd(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x + b.x, a.y + b.y);
+}
+
+static inline CGPoint rwSub(CGPoint a, CGPoint b) {
+    return CGPointMake(a.x - b.x, a.y - b.y);
+}
+
+static inline CGPoint rwMult(CGPoint a, float b) {
+    return CGPointMake(a.x * b, a.y * b);
+}
+
+static inline float rwLength(CGPoint a) {
+    return sqrtf(a.x * a.x + a.y * a.y);
+}
+
+// Makes a vector have a length of 1
+static inline CGPoint rwNormalize(CGPoint a) {
+    float length = rwLength(a);
+    return CGPointMake(a.x / length, a.y / length);
+}
 
 @implementation MyScene
 
 -(id)initWithSize:(CGSize)size {    
     if (self = [super initWithSize:size]) {
-        /* Setup your scene here */
+
+        NSLog(@"Size: %@",NSStringFromCGSize(size));
         
-        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
+        self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
         
-        SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+        self.player = [SKSpriteNode spriteNodeWithImageNamed:@"player"];
+        self.player.position = CGPointMake(self.player.size.width/2,self.frame.size.height/2);
+        [self addChild:self.player];
         
-        myLabel.text = @"Hello, World!";
-        myLabel.fontSize = 30;
-        myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                       CGRectGetMidY(self.frame));
+        self.physicsWorld.gravity = CGVectorMake(0,0);
+        self.physicsWorld.contactDelegate = self;
         
-        [self addChild:myLabel];
+
     }
     return self;
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
+
+- (void) addMonster {
+    SKSpriteNode * monster = [SKSpriteNode spriteNodeWithImageNamed:@"monster"];
     
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
+    int minY = monster.size.height/2;
+    int maxY = self.frame.size.height - monster.size.height /2;
+    int rangeY = maxY-minY;
+    int actualY = (arc4random() % rangeY)+minY;
+    
+    monster.position = CGPointMake(self.frame.size.width + monster.size.width / 2, actualY);
+    [self addChild:monster];
+    monster.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:monster.size]; // 1
+    monster.physicsBody.dynamic = YES; // 2
+    monster.physicsBody.categoryBitMask = monsterCategory; // 3
+    monster.physicsBody.contactTestBitMask = projectileCategory; // 4
+    monster.physicsBody.collisionBitMask = 0; // 5
+    
+    int minDuration = 2.0;
+    int maxDuration = 4.0;
+    int rangeDuration = maxDuration - minDuration;
+    int actualDuration = (arc4random() % rangeDuration) + minDuration;
+    
+    SKAction * actionMove = [SKAction moveTo:CGPointMake(-monster.size.width/2,actualY) duration:actualDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+
+    __weak typeof(self) weakSelf = self;
+    SKAction * loseAction = [SKAction runBlock:^{
+        SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
+        SKScene * gameOverScene = [[GameOverScene alloc] initWithSize:weakSelf.size won:NO];
+        [weakSelf.view presentScene:gameOverScene transition: reveal];
+    }];
+    
+    [monster runAction:[SKAction sequence:@[actionMove, loseAction, actionMoveDone]]];
+}
+
+-(void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    [self runAction:[SKAction playSoundFileNamed:@"pew-pew-lei.caf" waitForCompletion:NO]];
+    UITouch * touch = [touches anyObject];
+    CGPoint location = [touch locationInNode:self];
+    SKSpriteNode * projectile = [SKSpriteNode spriteNodeWithImageNamed:@"projectile"];
+    
+    projectile.position = self.player.position;
+    projectile.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:projectile.size.width/2];
+    projectile.physicsBody.dynamic = YES;
+    projectile.physicsBody.categoryBitMask = projectileCategory;
+    projectile.physicsBody.contactTestBitMask = monsterCategory;
+    projectile.physicsBody.collisionBitMask = 0;
+    projectile.physicsBody.usesPreciseCollisionDetection = YES;
+    
+    CGPoint offset = rwSub(location, projectile.position);
+    
+    if (offset.x <=0) return;
+    
+    [self addChild:projectile];
+    
+    CGPoint direction = rwNormalize(offset);
+    
+    CGPoint shootAmount = rwMult(direction, 1000);
+    projectile.zRotation = direction.y;
+    CGPoint realDest = rwAdd(shootAmount, projectile.position);
+    
+    float velocity = 480.0 / 1.0;
+    float realMoveDuration = self.size.width / velocity;
+    SKAction * actionMove =[SKAction moveTo:realDest duration:realMoveDuration];
+    SKAction * actionMoveDone = [SKAction removeFromParent];
+    [projectile runAction:[SKAction sequence:@[actionMove,actionMoveDone]]];
+     
+}
+
+- (void)projectile:(SKSpriteNode *)projectile didCollideWithMonster:(SKSpriteNode *)monster {
+    NSLog(@"Hit");
+    [projectile removeFromParent];
+    [monster removeFromParent];
+    self.monstersDestroyed++;
+    if (self.monstersDestroyed > 30) {
+        SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
+        SKScene * gameOverScene = [[GameOverScene alloc] initWithSize:self.size won:YES];
+        [self.view presentScene:gameOverScene transition: reveal];
     }
 }
 
--(void)update:(CFTimeInterval)currentTime {
-    /* Called before each frame is rendered */
+- (void)didBeginContact:(SKPhysicsContact *)contact
+{
+    SKPhysicsBody *firstBody, *secondBody;
+    
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask)
+    {
+        firstBody = contact.bodyA;
+        secondBody = contact.bodyB;
+    }
+    else
+    {
+        firstBody = contact.bodyB;
+        secondBody = contact.bodyA;
+    }
+    
+    if ((firstBody.categoryBitMask & projectileCategory) != 0 &&
+        (secondBody.categoryBitMask & monsterCategory) != 0)
+    {
+        [self projectile:(SKSpriteNode *) firstBody.node didCollideWithMonster:(SKSpriteNode *) secondBody.node];
+    }
+}
+
+
+
+-(void)updateWithTimeSinceLastUpdate:(CFTimeInterval)timeSinceLast {
+    self.lastSpawnTimeInterval += timeSinceLast;
+    if (self.lastSpawnTimeInterval >1){
+        self.lastSpawnTimeInterval = 0;
+        [self addMonster];
+    }
+}
+
+
+-(void)update:(NSTimeInterval)currentTime {
+    CFTimeInterval timeSinceLast = currentTime - self.lastUpdateInterval;
+    self.lastUpdateInterval = currentTime;
+    if (timeSinceLast >1){
+        timeSinceLast = 1.0 / 60.0;
+        self.lastUpdateInterval = currentTime;
+    }
+    [self updateWithTimeSinceLastUpdate:timeSinceLast];
 }
 
 @end
