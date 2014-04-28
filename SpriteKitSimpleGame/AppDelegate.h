947bce85c6364e30fb69d0482c4b1de0fc186ae4
Mirror Spock <mirrorspock@mirrorspocksmbp.fritz.box>
2014-04-28 16:53:29 +0200

    Initial Commit

---
+++ SpriteKitSimpleGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SpriteKitSimpleGame
+//
+//  Created by Mirror Spock on 28/04/14.
+//  Copyright (c) 2014 imkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
